89057b224c08a1f923bffc1821d22ea1d80cbcc2
plaristote <michael@unetresgrossebite.com>
2022-08-16 00:27:34 +0200

    initial commit

---
+++ libcrails-controllers/controller.hpp
@@ -0,0 +1,14 @@
+#ifndef  CRAILS_CONTROLLER_HPP
+# define CRAILS_CONTROLLER_HPP
+
+# include "controller/action.hpp"
+# include "controller/basic_authentication.hpp"
+# include "controller/render.hpp"
+# include "controller/csrf.hpp"
+
+namespace Crails
+{
+  typedef RenderController Controller;
+}
+
+#endif
